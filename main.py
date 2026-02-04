0eab0ba884fbb141c3eb075e63de158bb51aaf17
matheusado1 <matheus-alves@outlook.com.br>
2026-02-03 21:24:26 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+receitas = []
+despesas = []
 
+print("MENU FINANCEIRO")
+print("1 - Adicionar receita")
+print("2 - Adicionar despesa")
+print("3 - Ver saldo")
+
+
+opcao = input("Escolha uma opção: ")
+
+if opcao == "1":
+    valor = float(input("Digite o valor da receita: "))
+    print("Receita adicionada com sucesso!")
+
+elif opcao == "2":
+    valor = float(input("Digite o valor da despesa: "))
+   
+    print("Despesa adicionada com sucesso!")
+
+elif opcao == "3":
+    saldo = sum(receitas) - sum(despesas)
+    print(f"Saldo atual: R$ {saldo:.2f}")
