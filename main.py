aee69adf6cee789bb2af09934807e4c0ea60cd5e
matheusado1 <matheus-alves@outlook.com.br>
2026-02-05 18:00:38 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
+# Sistema Financeiro Pessoal 
+
 receitas = []
 despesas = []
 
-print("MENU FINANCEIRO")
-print("1 - Adicionar receita")
-print("2 - Adicionar despesa")
-print("3 - Ver saldo")
+while True:
+    print("\nMENU FINANCEIRO ")
+    print("1 - Adicionar receita")
+    print("2 - Adicionar despesa")
+    print("3 - Ver saldo")
 
+    opcao = input("Escolha uma opção: ")
 
-opcao = input("Escolha uma opção: ")
+    if opcao == "1":
+        valor = float(input("Digite o valor da receita: "))
+        receitas.append(valor)
+        print("Receita adicionada com sucesso!")
 
-if opcao == "1":
-    valor = float(input("Digite o valor da receita: "))
-    print("Receita adicionada com sucesso!")
+    elif opcao == "2":
+        valor = float(input("Digite o valor da despesa: "))
+        despesas.append(valor)
+        print("Despesa adicionada com sucesso!")
 
-elif opcao == "2":
-    valor = float(input("Digite o valor da despesa: "))
-   
-    print("Despesa adicionada com sucesso!")
-
-elif opcao == "3":
-    saldo = sum(receitas) - sum(despesas)
-    print(f"Saldo atual: R$ {saldo:.2f}")
+    elif opcao == "3":
+        saldo = sum(receitas) - sum(despesas)
+        print(f"Saldo atual: R$ {saldo:.2f}")
